a9fc2bd12c8620a5730ce6d6f839602ce4e9de3b
Ryan Barnard <rbarnard@arane.us>
2018-03-13 17:11:26 -0400

    Wrapped the R code in functions so that the package can be loaded.
    Replaced library(...) with package-relative function calls.

--- R/Data_RXnorm.R
+++ R/Data_RXnorm.R
@@ -1,13 +1,12 @@
-source("toDL.R")
-RXN_cui_Change<- db.q("select * from rxn.rxn_rxncuichanges;",nrows = NULL,verbose = FALSE)
-RXN_cui <- db.q("select * from rxn.rxn_rxncui;",nrows = NULL,verbose = FALSE)
-RXN_ConSo <- db.q("select * from rxn.rxn_rxnconso;",nrows = NULL,verbose = FALSE)
-RXN_archive <- db.q("select * from rxn.rxn_rxnatomarchive;",nrows = NULL,verbose = FALSE)
-RXN_rel <- db.q("select * from rxn.rxn_rxnrel;",nrows = NULL,verbose = FALSE)
+createRxNormDataFromDatabase <- function(conn) {
+   RXN_cui_Change<- db.q("select * from rxn.rxn_rxncuichanges;",nrows = NULL,verbose = FALSE)
+   RXN_cui <- db.q("select * from rxn.rxn_rxncui;",nrows = NULL,verbose = FALSE)
+   RXN_ConSo <- db.q("select * from rxn.rxn_rxnconso;",nrows = NULL,verbose = FALSE)
+   RXN_archive <- db.q("select * from rxn.rxn_rxnatomarchive;",nrows = NULL,verbose = FALSE)
+   RXN_rel <- db.q("select * from rxn.rxn_rxnrel;",nrows = NULL,verbose = FALSE)
 
-db.disconnect(conn)
+   db.disconnect(conn)
+   rm(conn)
 
-rm(conn)
-
-save.image("RXNORM.Rdata")
-
+   save.image("RXNORM.Rdata")
+}
--- R/Net_NDFRT.R
+++ R/Net_NDFRT.R
@@ -1,120 +1,158 @@
-library(plyr)
-library(igraph)
-load("NDFRT.Rdata")
+buildNdfrtNetworks <- function() {
+   load("NDFRT.Rdata")
 
-Ontology.NDFRT <- function(kind, DC = Def_Concept,DFC = DefiningConcepts) {
-  Ontology <- vector("list",3)
-  names(Ontology) <- c("Nodes","Arcs","top")
-  Nodes <- DC[DC$kind == kind,c(2,2,1)]
-  Nodes <- cbind(Nodes,kind = kind)
-  rownames(Nodes) <- NULL
-  names(Nodes) <- c("id","code","c_name","kind") 
-  is.a  <- subset(DFC, DFC$concept_code %in% Nodes$code)
-  rownames(is.a) <- NULL
-  names(is.a) <- c("from","to")
-  net <- graph_from_data_frame(d=is.a, vertices=Nodes, directed=T) 
-  Ontology$top <- names(which(degree(net,mode = "out") == 0))
-  Ontology$Nodes <- Nodes
-  Ontology$Arcs <- is.a
-  return(Ontology)
+   Ontology.NDFRT <- function(kind, DC = Def_Concept,DFC = DefiningConcepts) {
+      Ontology <- vector("list",3)
+      names(Ontology) <- c("Nodes","Arcs","top")
+      Nodes <- DC[DC$kind == kind,c(2,2,1)]
+      Nodes <- cbind(Nodes,kind = kind)
+      rownames(Nodes) <- NULL
+      names(Nodes) <- c("id","code","c_name","kind") 
+      is.a  <- subset(DFC, DFC$concept_code %in% Nodes$code)
+      rownames(is.a) <- NULL
+      names(is.a) <- c("from","to")
+      net <- igraph::graph_from_data_frame(d=is.a, vertices=Nodes, directed=T) 
+      Ontology$top <- names(which(igraph::degree(net,mode = "out") == 0))
+      Ontology$Nodes <- Nodes
+      Ontology$Arcs <- is.a
+      return(Ontology)
+   }
+
+   DrugTo <- function(LinkCodes = NA, Exclude = c("C30","C36","C26","C22","C52") ,DR = DefiningRoles,DRV =Def_RoleValue) {
+      if (all(is.na(LinkCodes))) {
+         X <- subset(DRV,!(DRV$name %in% Exclude))
+      } else {
+         X <- subset(DRV,DRV$name %in% LinkCodes)
+      }
+
+      from <- to <- character(0)
+      for ( i in 1:nrow(X)) {
+         cL <- DR$concept_code[DR$role_value_ids == X$rid[i]]
+         from <- c(from,cL); 
+         to <- c(to,rep(X$value[i],length(cL)))
+      }
+
+      Y <- data.frame(from =from, to = to, stringsAsFactors = F)
+
+      return(Y)
+   }
+
+
+   Node2Top <- function(CUI, net = net_Pan, TopC = Tops) {
+      ridx <- 1
+      AllNodes <- NA
+
+      while (is.na(AllNodes[1])) {
+         paths <- all_simple_paths(net,CUI,TopC[ridx],"out")  
+
+         if (length(paths) > 0 ) {
+            AllNodes <- paths[[1]]
+         } else {
+            ridx <- ridx+1
+         }
+      }
+
+      if (length(paths) > 1 ) {
+         for (j in 2:length(paths)) {
+            AllNodes <- union(AllNodes,paths[[j]]) 
+         }
+      }
+      
+      for (i in TopC[-(1:ridx)]) {
+         paths <- all_simple_paths(net,CUI,i,"out")  
+
+         if (length(paths) > 0) {
+            for (j in 1:length(paths)) {
+               AllNodes <- union(AllNodes,paths[[j]])
+            }
+         }
+      }
+
+      return(AllNodes)
+   }
+
+
+   ######################################## DRUG kind in NDFRT is not an Ontotlogy
+
+
+   NDFRT.Drug  <- vector("list",4)
+   names(NDFRT.Drug) <- c("Nodes","Arcs","is.a","association")
+   NDFRT.Drug$Nodes <- data.frame(id = Rx_Norm.NDFRT$concept_code, code = Rx_Norm.NDFRT$concept_code,
+                                 c_name = Rx_Norm.NDFRT$concept_name,stringsAsFactors = F)
+   NDFRT.Drug$is.a <- subset(DefiningConcepts,  DefiningConcepts$concept_code %in% NDFRT.Drug$Nodes$code)
+   names(NDFRT.Drug$is.a ) <- c("from","to")
+   NDFRT.Drug$association <-  subset(ConceptAssociation, ConceptAssociation$value %in% NDFRT.Drug$Nodes$code)[,c(1,3,4)] 
+
+   #Links.Assoc <- rbind(NDFRT.Drug$association[,c(1,2)],NDFRT.Drug$association[,c(2,1)])
+   Links.Assoc <- NDFRT.Drug$association[,c(1,2)]
+   names(Links.Assoc) <- c("from","to")
+
+   NDFRT.Drug$Arcs <- unique(rbind(NDFRT.Drug$is.a,Links.Assoc))
+   rownames(NDFRT.Drug$Arcs) <- NULL
+
+
+   #################### Create Ontology of other Kind
+
+
+   Ont.MoA <- Ontology.NDFRT("C12")
+   Ont.DISEASE <- Ontology.NDFRT("C16")
+   Ont.INGREDIENT <- Ontology.NDFRT("C10")
+   Ont.DOSE_FORM <- Ontology.NDFRT("C18")
+   Ont.THERAPEUTIC_CATEGORY <- Ontology.NDFRT("K20")
+   Ont.PHARMACOKINETICS <- Ontology.NDFRT("C14")
+   Ont.PHYSIOLOGIC_EFFECT <- Ontology.NDFRT("C6")
+
+
+   save(
+      NDFRT.Drug,
+      Ont.MoA,
+      Ont.DISEASE,
+      Ont.INGREDIENT,
+      Ont.DOSE_FORM,
+      Ont.THERAPEUTIC_CATEGORY,
+      Ont.PHARMACOKINETICS,
+      Ont.PHYSIOLOGIC_EFFECT,
+      file = "Ontologies.Rdata"
+   )
+
+
+   Tops <- c(
+      Ont.MoA$top,
+      Ont.DISEASE$top,
+      Ont.DOSE_FORM$top,
+      Ont.INGREDIENT$top,
+      Ont.PHARMACOKINETICS$top,
+      Ont.PHYSIOLOGIC_EFFECT$top,
+      Ont.THERAPEUTIC_CATEGORY$top
+   )
+
+
+   ### pan net ## without CI_*
+
+   Nodes_all <- Def_Concept[,c(2,2,1,6)]
+   names(Nodes_all) <-  c("id","code","c_name","kind") 
+   Links_all <- DrugTo()
+
+   is.a_all <- DefiningConcepts
+   names(is.a_all) <- c("from","to")
+
+   net_Pan  <- igraph::graph_from_data_frame(
+      d=unique(rbind(NDFRT.Drug$Arcs,Links_all,is.a_all)), 
+      vertices=Nodes_all,
+      directed=T
+   )
+
+   save(Tops,net_Pan,file = "Pan_net.Rdata")
+
+
+   ## NET_Drug_MOA : has_MoA  
+
+   LinkCodes <- Def_Role$code[grep("has_MoA",Def_Role$name)]
+
+   k <- DrugTo(LinkCodes=LinkCodes) 
+   net_MoA_ <- igraph::graph_from_data_frame(
+      d=unique(rbind(NDFRT.Drug$Arcs,DrugTo(LinkCodes=LinkCodes),Ont.MoA$Arc)), 
+      vertices=rbind(NDFRT.Drug$Nodes,Ont.MoA$Nodes[,1:3]),
+      directed=T
+   )
 }
-
-DrugTo <- function(LinkCodes = NA, Exclude = c("C30","C36","C26","C22","C52") ,DR = DefiningRoles,DRV =Def_RoleValue){
-  if (all(is.na(LinkCodes)))   X <- subset(DRV,!(DRV$name %in% Exclude)) else 
-    X <- subset(DRV,DRV$name %in% LinkCodes)
-  from <- to <- character(0)
-  for ( i in 1:nrow(X)){
-    cL <- DR$concept_code[DR$role_value_ids == X$rid[i]]
-    from <- c(from,cL); 
-    to <- c(to,rep(X$value[i],length(cL)))
-  }
-  Y <- data.frame(from =from, to = to, stringsAsFactors = F)
-  return(Y)
-}
-
-
-Node2Top <- function(CUI, net = net_Pan, TopC = Tops) {
-  ridx <- 1
-  AllNodes <- NA
-  while (is.na(AllNodes[1])) {
-    paths <- all_simple_paths(net,CUI,TopC[ridx],"out")  
-    if (length(paths) > 0 )  AllNodes <- paths[[1]] else ridx <- ridx+1
-  }
-  if (length(paths) > 1 )  for (j in 2:length(paths)) AllNodes <- union(AllNodes,paths[[j]]) 
-  
-  for (i in TopC[-(1:ridx)]) {
-    paths <- all_simple_paths(net,CUI,i,"out")  
-    if (length(paths) > 0)  for (j in 1:length(paths)) AllNodes <- union(AllNodes,paths[[j]])   
-  }
-  return(AllNodes)
-}
-
-
-
-
-######################################## DRUG kind in NDFRT is not an Ontotlogy
-
-
-NDFRT.Drug  <- vector("list",4)
-names(NDFRT.Drug) <- c("Nodes","Arcs","is.a","association")
-NDFRT.Drug$Nodes <- data.frame(id = Rx_Norm.NDFRT$concept_code, code = Rx_Norm.NDFRT$concept_code,
-                               c_name = Rx_Norm.NDFRT$concept_name,stringsAsFactors = F)
-NDFRT.Drug$is.a <- subset(DefiningConcepts,  DefiningConcepts$concept_code %in% NDFRT.Drug$Nodes$code)
-names(NDFRT.Drug$is.a ) <- c("from","to")
-NDFRT.Drug$association <-  subset(ConceptAssociation, ConceptAssociation$value %in% NDFRT.Drug$Nodes$code)[,c(1,3,4)] 
-
-#Links.Assoc <- rbind(NDFRT.Drug$association[,c(1,2)],NDFRT.Drug$association[,c(2,1)])
-Links.Assoc <- NDFRT.Drug$association[,c(1,2)]
-names(Links.Assoc) <- c("from","to")
-
-NDFRT.Drug$Arcs <- unique(rbind(NDFRT.Drug$is.a,Links.Assoc))
-rownames(NDFRT.Drug$Arcs) <- NULL
-
-
-#################### Create Ontology of other Kind
-
-
-Ont.MoA <- Ontology.NDFRT("C12")
-Ont.DISEASE <- Ontology.NDFRT("C16")
-Ont.INGREDIENT <- Ontology.NDFRT("C10")
-Ont.DOSE_FORM <- Ontology.NDFRT("C18")
-Ont.THERAPEUTIC_CATEGORY <- Ontology.NDFRT("K20")
-Ont.PHARMACOKINETICS <- Ontology.NDFRT("C14")
-Ont.PHYSIOLOGIC_EFFECT <- Ontology.NDFRT("C6")
-
-
-save(NDFRT.Drug,Ont.MoA,Ont.DISEASE,Ont.INGREDIENT,Ont.DOSE_FORM,Ont.THERAPEUTIC_CATEGORY,Ont.PHARMACOKINETICS,Ont.PHYSIOLOGIC_EFFECT,
-     file = "Ontologies.Rdata")
-
-
-
-Tops <- c(Ont.MoA$top,Ont.DISEASE$top,Ont.DOSE_FORM$top,Ont.INGREDIENT$top,
-          Ont.PHARMACOKINETICS$top,Ont.PHYSIOLOGIC_EFFECT$top,Ont.THERAPEUTIC_CATEGORY$top)
-
-
-### pan net ## without CI_*
-
-Nodes_all <- Def_Concept[,c(2,2,1,6)]
-names(Nodes_all) <-  c("id","code","c_name","kind") 
-Links_all <- DrugTo()
-
-is.a_all <- DefiningConcepts
-names(is.a_all) <- c("from","to")
-
-net_Pan  <- graph_from_data_frame(d=unique(rbind(NDFRT.Drug$Arcs,Links_all,is.a_all)), 
-                                  vertices= Nodes_all, directed=T)
-
-save(Tops,net_Pan,file = "Pan_net.Rdata")
-
-
-## NET_Drug_MOA : has_MoA  
-
-LinkCodes <- Def_Role$code[grep("has_MoA",Def_Role$name)]
-
-k <- DrugTo(LinkCodes=LinkCodes) 
-net_MoA_ <-graph_from_data_frame(d=unique(rbind(NDFRT.Drug$Arcs,DrugTo(LinkCodes=LinkCodes),Ont.MoA$Arc)), 
-                                vertices= rbind(NDFRT.Drug$Nodes,Ont.MoA$Nodes[,1:3]), directed=T)
-
-
-
-
