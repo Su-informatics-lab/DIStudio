8ac9b01771b44df2731944ae0f841c042460fb7a
Ryan Barnard <rbarnard@arane.us>
2018-03-13 17:26:46 -0400

    Tidied up some formatting.

--- R/Data_NDFRT.R
+++ R/Data_NDFRT.R
@@ -17,25 +17,44 @@ createNdfrtDataFromDatabase <- function(conn) {
 
    # Modify DefiningRoles data 
    DefiningRoles <- plyr::ddply(DefiningRoles, plyr::.(concept_code),.fun = function(x) {
-   idx <- which(sapply(x$role_value_ids,nchar) > 5)
-   if (length(idx) > 0) {
-      y <- x$role_value_ids[-idx]
-      for (i in idx)  y <- union(y,unlist(strsplit(x$role_value_ids[i],"|",fixed =T)))
-      Y <- data.frame(concept_code =rep(x$concept_code[1],length(y)), 
-                     role_value_ids = y, stringsAsFactors = F )
-   } else Y <- x
-   return(Y)
-   }
-   )
+      idx <- which(sapply(x$role_value_ids,nchar) > 5)
+
+      if (length(idx) > 0) {
+         y <- x$role_value_ids[-idx]
+
+         for (i in idx) {
+            y <- union(y,unlist(strsplit(x$role_value_ids[i],"|",fixed =T)))
+         }
+
+         Y <- data.frame(
+            concept_code =rep(x$concept_code[1],length(y)), 
+            role_value_ids = y,
+            stringsAsFactors = F
+         )
+      } else {
+         Y <- x
+      }
+
+      return(Y)
+   })
 
    RX_Norm <- plyr::ddply(ConceptProperty, plyr::.(concept_code),.fun = function(x) {
-   idx <- match(c("C818","C819","C10490107818172"),x$name)
-   if (! all(is.na(idx))) return(c(RX_CUI = x$value[idx[1]], RX_name = x$value[idx[2]],NUI =x$value[idx[3]]  ))}   
+      idx <- match(c("C818","C819","C10490107818172"), x$name)
+
+      if (! all(is.na(idx))) {
+         return(c(
+            RX_CUI = x$value[idx[1]],
+            RX_name = x$value[idx[2]],
+            NUI =x$value[idx[3]]
+         ))
+      }
+   })
+
+   Rx_Norm.NDFRT <- subset(RX_Norm, RX_Norm$concept_code %in% Def_Concept$code[Def_Concept$kind == "C8"])
+   Rx_Norm.NDFRT <- cbind(
+      concept_name = Def_Concept$name[match(Rx_Norm.NDFRT$concept_code,Def_Concept$code)],
+      Rx_Norm.NDFRT
    )
-
-   Rx_Norm.NDFRT <- subset(RX_Norm, RX_Norm$concept_code %in% Def_Concept$code[Def_Concept$kind =="C8"])
-   Rx_Norm.NDFRT <- cbind(concept_name =Def_Concept$name[match(Rx_Norm.NDFRT$concept_code,Def_Concept$code)],Rx_Norm.NDFRT)
-
 
    save.image("NDFRT.Rdata")
 }
