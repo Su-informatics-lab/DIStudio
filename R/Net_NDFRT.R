31576fbd0184062f5dfcc2a9aa05e2558cddd62c
Ryan Barnard <rbarnard@arane.us>
2018-03-26 16:10:31 -0400

    Replaced previous Net_NDFRT with Shyh-Huei's latest code.

--- R/Net_NDFRT.R
+++ R/Net_NDFRT.R
@@ -1,169 +1,196 @@
-Ontology.NDFRT <- function(kind, DC, DFC) {
-   Ontology <- vector("list", 3)
-   names(Ontology) <- c("Nodes", "Arcs", "top")
+library(plyr)
+library(igraph)
+load("NDFRT.Rdata")
 
-   Nodes <- DC[DC$kind == kind, c(2, 2, 1)]
-   Nodes <- cbind(Nodes, kind = kind)
-   rownames(Nodes) <- NULL
-   names(Nodes) <- c("id", "code", "c_name", "kind") 
 
-   is.a  <- subset(DFC, DFC$concept_code %in% Nodes$code)
-   rownames(is.a) <- NULL
-   names(is.a) <- c("from", "to")
-
-   net <- igraph::graph_from_data_frame(d=is.a, vertices=Nodes, directed=T) 
-
-   Ontology$top <- names(which(igraph::degree(net, mode = "out") == 0))
-   Ontology$Nodes <- Nodes
-   Ontology$Arcs <- is.a
-
-   return(Ontology)
-}
-
-DrugTo <- function(LinkCodes, DR, DRV, Exclude=c("C30", "C36", "C26", "C22", "C52")) {
-   if (all(is.na(LinkCodes))) {
-      X <- subset(DRV, !(DRV$name %in% Exclude))
-   } else {
-      X <- subset(DRV, DRV$name %in% LinkCodes)
-   }
-
-   from <- to <- character(0)
-   for (i in 1:nrow(X)) {
-      cL <- DR$concept_code[DR$role_value_ids == X$rid[i]]
-      from <- c(from, cL); 
-      to <- c(to, rep(X$value[i], length(cL)))
-   }
-
-   Y <- data.frame(from=from , to=to, stringsAsFactors=F)
-
-   return(Y)
+##  not for Drug kind
+##  define is.a "R11" (all kinds), asociation "R12" (drug)
+Ontology.NDFRT <- function(kind, DC = Def_Concept,DFC = DefiningConcepts) {
+  Ontology <- vector("list",3)
+  names(Ontology) <- c("Nodes","Arcs","top")
+  Nodes <- DC[DC$kind == kind,c(2,2,1)]
+  Nodes <- cbind(Nodes,kind = kind)
+  rownames(Nodes) <- NULL
+  names(Nodes) <- c("id","code","c_name","kind") 
+  is.a  <- subset(DFC, DFC$concept_code %in% Nodes$code)
+  rownames(is.a) <- NULL
+  names(is.a) <- c("from","to")
+  is.a$rel <- "is.a"
+  is.a$rel.code <- "R11"
+  net <- graph_from_data_frame(d=is.a, vertices=Nodes, directed=T) 
+  Ontology$top <- names(which(degree(net,mode = "out") == 0))
+  Ontology$Nodes <- Nodes
+  Ontology$Arcs <- is.a
+  return(Ontology)
 }
 
 
-Node2Top <- function(CUI, net, TopC) {
-   ridx <- 1
-   AllNodes <- NA
+## Drug ontology 
 
-   while (is.na(AllNodes[1])) {
-      paths <- all_simple_paths(net, CUI, TopC[ridx], "out")  
-
-      if (length(paths) > 0 ) {
-         AllNodes <- paths[[1]]
-      } else {
-         ridx <- ridx + 1
-      }
-   }
-
-   if (length(path) > 1) {
-      for (j in 2:length(paths)) {
-         AllNodes <- union(AllNodes, paths[[j]]) 
-      }
-   }
-   
-   for (i in TopC[-(1:ridx)]) {
-      paths <- all_simple_paths(net, CUI, i, "out")  
-
-      if (length(paths) > 0) {
-         for (j in 1:length(paths)) {
-            AllNodes <- union(AllNodes, paths[[j]])
-         }
-      }
-   }
-
-   return(AllNodes)
+Ontology.Drug <- function( rxnorm = Rx_Norm.NDFRT, is.a = DefiningConcepts, association = ConceptAssociation){
+  Drug  <- vector("list",3)
+  names(Drug) <- c("Nodes","is.a","association")
+  Drug$Nodes <- data.frame(id = rxnorm$concept_code, code = rxnorm$concept_code,
+                           c_name = rxnorm$concept_name, kind = "C8", stringsAsFactors = F)
+  
+  Drug$is.a <- subset(is.a,  is.a$concept_code %in% Drug$Nodes$code)
+  names(Drug$is.a ) <- c("from","to")
+  Drug$is.a$rel <- "is.a"
+  Drug$is.a$rel.code <- "R11"
+  
+  Drug$association <-  subset(association, association$value %in% Drug$Nodes$code)[,c(1,3)] 
+  names(Drug$association)[1:2] <- c("from","to")
+  Drug$association$rel <- "association"
+  Drug$association$rel.code <- "R12"
+  
+  net <- graph_from_data_frame(d=rbind(Drug$is.a,Drug$association), vertices=Drug$Nodes, directed=T) 
+  Drug$top <- names(which(degree(net,mode = "out") == 0))
+  return(Drug)
 }
 
 
-buildNdfrtNetworks <- function() {
-   load("NDFRT.Rdata")
-
-
-   ######################################## DRUG kind in NDFRT is not an Ontology
-
-   NDFRT.Drug  <- vector("list", 4)
-   names(NDFRT.Drug) <- c("Nodes", "Arcs", "is.a", "association")
-
-   NDFRT.Drug$Nodes <- data.frame(
-      id=Rx_Norm.NDFRT$concept_code,
-      code=Rx_Norm.NDFRT$concept_code,
-      c_name=Rx_Norm.NDFRT$concept_name,
-      stringsAsFactors=F
-   )
-
-   NDFRT.Drug$is.a <- subset(DefiningConcepts, DefiningConcepts$concept_code %in% NDFRT.Drug$Nodes$code)
-   names(NDFRT.Drug$is.a) <- c("from", "to")
-
-   NDFRT.Drug$association <- subset(ConceptAssociation, ConceptAssociation$value %in% NDFRT.Drug$Nodes$code)[, c(1, 3, 4)] 
-
-   #Links.Assoc <- rbind(NDFRT.Drug$association[,c(1,2)],NDFRT.Drug$association[,c(2,1)])
-   Links.Assoc <- NDFRT.Drug$association[, c(1, 2)]
-   names(Links.Assoc) <- c("from", "to")
-
-   NDFRT.Drug$Arcs <- unique(rbind(NDFRT.Drug$is.a, Links.Assoc))
-   rownames(NDFRT.Drug$Arcs) <- NULL
-
-
-   #################### Create Ontology of other Kind
-
-   Ont.MoA <- Ontology.NDFRT("C12", Def_Concept, DefiningConcepts)
-   Ont.DISEASE <- Ontology.NDFRT("C16", Def_Concept, DefiningConcepts)
-   Ont.INGREDIENT <- Ontology.NDFRT("C10", Def_Concept, DefiningConcepts)
-   Ont.DOSE_FORM <- Ontology.NDFRT("C18", Def_Concept, DefiningConcepts)
-   Ont.THERAPEUTIC_CATEGORY <- Ontology.NDFRT("K20", Def_Concept, DefiningConcepts)
-   Ont.PHARMACOKINETICS <- Ontology.NDFRT("C14", Def_Concept, DefiningConcepts)
-   Ont.PHYSIOLOGIC_EFFECT <- Ontology.NDFRT("C6", Def_Concept, DefiningConcepts)
-
-   save(
-      NDFRT.Drug,
-      Ont.MoA,
-      Ont.DISEASE,
-      Ont.INGREDIENT,
-      Ont.DOSE_FORM,
-      Ont.THERAPEUTIC_CATEGORY,
-      Ont.PHARMACOKINETICS,
-      Ont.PHYSIOLOGIC_EFFECT,
-      file = "Ontologies.Rdata"
-   )
-
-
-   Tops <- c(
-      Ont.MoA$top,
-      Ont.DISEASE$top,
-      Ont.DOSE_FORM$top,
-      Ont.INGREDIENT$top,
-      Ont.PHARMACOKINETICS$top,
-      Ont.PHYSIOLOGIC_EFFECT$top,
-      Ont.THERAPEUTIC_CATEGORY$top
-   )
-
-
-   ### pan net ## without CI_*
-
-   Nodes_all <- Def_Concept[, c(2, 2, 1, 6)]
-   names(Nodes_all) <-  c("id", "code", "c_name", "kind") 
-   
-   Links_all <- DrugTo(LinkCodes=NA, DR=DefiningRoles, DRV=Def_RoleValue)
-
-   is.a_all <- DefiningConcepts
-   names(is.a_all) <- c("from", "to")
-
-   net_Pan <- igraph::graph_from_data_frame(
-      d=unique(rbind(NDFRT.Drug$Arcs, Links_all, is.a_all)), 
-      vertices=Nodes_all,
-      directed=T
-   )
-
-
-   ## NET_Drug_MOA : has_MoA  
-
-   LinkCodes <- Def_Role$code[grep("has_MoA", Def_Role$name)]
-
-   k <- DrugTo(LinkCodes=LinkCodes, DR=DefiningRoles, DRV=Def_RoleValue) 
-   net_MoA_ <- igraph::graph_from_data_frame(
-      d=unique(rbind(NDFRT.Drug$Arcs, DrugTo(LinkCodes=LinkCodes, DR=DefiningRoles, DRV=Def_RoleValue), Ont.MoA$Arc)), 
-      vertices=rbind(NDFRT.Drug$Nodes, Ont.MoA$Nodes[, 1:3]),
-      directed=T
-   )
-
-   list(pan=net_Pan, moa=net_MoA_)
+# find in-between relations and generate links
+DrugTo <- function(LinkCodes = NA, Exclude = c("C30","C36","C26","C22","C52") ,DR = DefiningRoles,DRV =Def_RoleValue, DefR = Def_Role){
+  if (all(is.na(LinkCodes)))   X <- subset(DRV,!(DRV$name %in% Exclude)) else 
+    X <- subset(DRV,DRV$name %in% LinkCodes)
+  from <- to <- rel <- rel.code <- character(0)
+  for ( i in 1:nrow(X)){
+    cL <- DR$concept_code[DR$role_value_ids == X$rid[i]]
+    from <- c(from,cL); 
+    to <- c(to,rep(X$value[i],length(cL)))
+    rel.code <- c(rel.code,rep(X$name[i],length(cL)))
+    rel <- c(rel,rep(DefR$name[which(DefR$code == X$name[i])],length(cL)))
+  }
+  Y <- data.frame(from =from, to = to, rel = rel, rel.code = rel.code, stringsAsFactors = F)
+  return(Y)
 }
+
+
+# find upstream and downstream Nodes (or subgraph)
+# kinds: which kinds notes
+# subgraph or nodes (igraph)
+find_nodes <- function(CUI, net , UD = "both", kinds = "All", SubGraph = F, NodeOnly = F) {
+  if (UD == "both") sc <- union(subcomponent(net,CUI,"out"),subcomponent(net,CUI,"in")) 
+  if (UD == "up")   sc <- subcomponent(net,CUI,"out")
+  if (UD == "down")   sc <- subcomponent(net,CUI,"in")
+  if (length(sc) ==1)  return(NA) else {
+    if (kinds[1] != "All" )  sc <- subset(sc,sc$kind %in% kinds)
+    if (SubGraph)  return(induced_subgraph(net,sc)) else if (NodeOnly) return(sc$code) else return(sc)
+  }}
+
+
+
+## Graph of single ontology 
+Ontology2Graph <- function(Ont, Drug.rel = "is.a" ){
+  if (Ont$Nodes$kind[1]  != "C8") {
+    return(graph_from_data_frame(d = Ont$Arcs, vertices= Ont$Nodes, directed=T))
+  } else {
+   if (Drug.rel == "both" ) return(graph_from_data_frame(d = rbind(Ont$is.a,Ont$association), vertices= Ont$Nodes, directed=T))
+   if (Drug.rel == "is.a" ) return(graph_from_data_frame(d = Ont$is.a, vertices= Ont$Nodes, directed=T))
+   if (Drug.rel == "association" ) return(graph_from_data_frame(d = Ont$association, vertices= Ont$Nodes, directed=T))
+  }}
+
+
+## Graph of Ontologies with Drug 
+Ontologies2Graph <- function(Onts, LinkCodes = NA, Drug = Ont.Drug, Drug.rel = "both"){
+  nOnts <- length(Onts)
+  nodes <- Drug$Nodes
+  if (Drug.rel == "is.a" ) arcs <- Drug$is.a else if (Drug.rel == "association") arcs <- Drug$association else arcs <- rbind(Drug$is.a,Drug$association)
+  if (is.na(LinkCodes[1])) arcs <- rbind(arcs,DrugTo()) else arcs <- rbind(arcs,DrugTo(LinkCodes))
+  for (i in 1:nOnts)  {
+    nodes <- rbind(nodes,Onts[[i]]$Nodes)
+    arcs <- rbind(arcs,Onts[[i]]$Arcs)
+  }
+  return(graph_from_data_frame(d = arcs, vertices= nodes, directed=T))
+}  
+
+
+
+view.network <- function(Graph, showdata =T){
+  nV <- length(V(Graph))
+  V(Graph)$PID <- 1:nV
+  vs  <-  15;  vlc <- .8;aw <- 1; ew <- 1;  eas <- 0.8
+  if (nV > 30) { vs  <-  vs *2/3;  vlc <- vlc*2/3;aw <- aw *2/3; ew <- ew*2/3;  eas <- eas*2/3;}
+  if (nV > 60) { vs  <-  vs/2;  vlc <- vlc/2;aw <- aw/2;ew <- ew/2;  eas <- eas/2;}  
+  if (showdata) {
+    plot(Graph, vertex.size = vs, vertex.color = factor(V(Graph)$kind),vertex.label.cex=vlc,
+         vertex.label.color= "black",vertex.label = V(Graph)$PID,
+         edge.width = ew, edge.arrow.size = eas, edge.arrow.width= aw, edge.color = factor(E(Graph)$rel.code),
+         edge.label = ifelse(E(Graph)$rel.code %in% c("R11","R12"),NA,E(Graph)$rel.code))
+    Graph.data <- as_data_frame(Graph,"both")
+    edges.label <- unique(Graph.data$edges[,c("rel","rel.code")])
+    rownames(edges.label) <- NULL
+    names(edges.label) <- c("Relation","Code")
+    nodes.label <- Graph.data$vertices[,c("PID","code","c_name")]
+    rownames(nodes.label) <- NULL
+    View(nodes.label)
+    View(edges.label)
+    } else plot(Graph, vertex.size = vs, vertex.color = factor(V(Graph)$kind),vertex.label.cex=vlc,
+                vertex.label.color= "black",edge.width = ew, edge.arrow.size = eas, edge.arrow.width= aw, edge.color = factor(E(Graph)$rel.code),
+                edge.label = ifelse(E(Graph)$rel.code %in% c("R11","R12"),NA,E(Graph)$rel.code))
+      
+}
+  
+
+## look up for code or name 
+LU_name.NDFRT <- function(x,ViewOnly =F) if (ViewOnly) View(Def_Concept[grep(x, Def_Concept$name, ignore.case=T),]) else return(Def_Concept[grep(x, Def_Concept$name, ignore.case=T),])
+LU_code.NDFRT <- function(x,ViewOnly =F) if (ViewOnly) View(subset(Def_Concept,Def_Concept$code %in% x)) else return(subset(Def_Concept,Def_Concept$code %in% x))
+
+
+
+## 
+Frequencies <- function(cui.list,cui.counts = NULL, net, Counts=NULL) {
+  kinds <- unique(V(net)$kind)
+  if (length(kinds) > 1)  kinds <- kinds[!(kinds == "C8")]
+  if (is.null(Counts))  { 
+    Counts <- as_data_frame(induced_subgraph(net,V(net)[V(net)$kind == kinds]),"both")
+    Counts$vertices$count <- 0
+    Counts$edges$count <- 0
+    Counts$edges$fromto <- paste0(Counts$edges$from,Counts$edges$to)
+    Counts$length <- 0
+    }
+  if (is.null(cui.counts))  cui.counts <- rep(1,length(cui.list))
+  for ( i in 1:length(cui.list)){
+    NodeArc <- as_data_frame(find_nodes(cui.list[i],net,UD = "up", kinds, SubGraph = T),"both")
+    if (!is.na(NodeArc$vertices[1,1])){
+    NodeArc$edges$fromto <- paste0(NodeArc$edges$from,NodeArc$edges$to)
+    NodeMatch <- match(NodeArc$vertices$code,Counts$vertices$code)
+    Counts$vertices$count[NodeMatch] <- Counts$vertices$count[NodeMatch]  + cui.counts[i]
+    ArcMatch <- match(NodeArc$edges$fromto,Counts$edges$fromto)
+    Counts$edges$count[ArcMatch] <- Counts$edges$count[ArcMatch] + cui.counts[i]
+    Counts$length <- Counts$length +1
+  }}
+  return(Counts)
+}
+
+
+## two CUIs from same kinds no need to consider arcs -- need eroor prevention later 
+Similarity <- function(cui1,cui2,net, weight = NULL, PD = F,View =F) {
+  kinds <- unique(V(net)$kind)
+  if (length(kinds) > 1)  kinds <- kinds[!(kinds == "C8")]
+  nodes1  <- find_nodes(cui1,net,UD = "up", kinds, SubGraph = F,NodeOnly = T)
+  nodes2  <- find_nodes(cui2,net,UD = "up", kinds, SubGraph = F,NodeOnly = T)
+  Int <- intersect(nodes1,nodes2)
+  Uni <- union(nodes1,nodes2)
+  if (View)  { par(mfrow=c(2,2))
+    plot(induced_subgraph(net,nodes1),main = cui1)
+    plot(induced_subgraph(net,nodes2),main = cui2)
+    plot(induced_subgraph(net,Int), main = "Intersection")
+    plot(induced_subgraph(net,Uni), main = "Union")
+  }
+  if (is.null(weight)) return(length(Int)/length(Uni)) else
+  { 
+    if(!PD) return(sum(weight$dist[match(Int,weight$code)])/sum(weight$dist[match(Uni,weight$code)])) else 
+      return(sum(2^weight$dist[match(Int,weight$code)])/sum(2^weight$dist[match(Uni,weight$code)])) }
+}
+  
+
+
+
+
+## 
+## coord <- layout.kamada.kawai(g)
+## coord2 <- layout.drl(g, use.seed=TRUE, seed=coord)
+## coord3 <- layout.drl(g, use.seed=TRUE, seed=coord)
+## plot(g,layout=coord2)
+## plot(g,layout=coord3)
