b0cbe2913ace85a7c04965575ba8e93f5a649314
Ryan Barnard <rbarnard@arane.us>
2018-03-27 17:40:17 -0400

    Added a helper function to create a self-contained model of a subset of the full data model for test purposes. (Data model is still evolving.)

--- R/util.R
+++ R/util.R
@@ -1,1 +1,18 @@
 .PACKAGE_NAME <- 'DIStudio'
+
+
+# Helper function for getting a subset of the full data for creating
+# focused unit tests
+getDataSubset <- function(dataModel, nodes, order) {
+   G <- dataModel$graph
+   verts <- names(igraph::ego(G, order, nodes=nodes)[[1]])
+   sg <- igraph::induced_subgraph(G, verts)
+
+   DefConcept <- dataModel$DefConcept
+   DefConcept <- DefConcept[DefConcept$code %in% verts, ]
+
+   list(
+      graph=sg,
+      DefConcept=DefConcept
+   )
+}
